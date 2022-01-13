0b119573158330b4fd9b60920848354b22f94914
Ramiro Nehuen <rnbarreiroc@gmail.com>
2022-01-13 19:33:48 -0300

    Ahora si todos los métodos funcionando a partir del archivo - restan incovenientes con el metodo save

--- desafio4.js
+++ desafio4.js
@@ -21,47 +21,84 @@ class Container {
         
         const productsText = JSON.stringify(this.productSave);
 
-        fs.promises.writeFile(`./${this.archiveName}`, productsText)
+        try { fs.promises.writeFile(`./${this.archiveName}`, productsText)
+        }
+        catch (err) {
+            console.log('No se pudo escribir el archivo!')
+        }
     };
 
     getById(givenId){
-        const productById = this.productSave.find(product => product.id === givenId)
-        console.log(productById)
+        fs.promises.readFile(`./${this.archiveName}`, 'utf-8')
+        .then(content => {
+            this.productSave = JSON.parse(content);
+            const productById = this.productSave.find(product => product.id === givenId)
+            console.log(productById)
+        })
+        .catch(err => {console.log('Error de Lectura', err)})
+
+        
     };
 
     getAll(){
-        console.log(this.productSave);
+        fs.promises.readFile(`./${this.archiveName}`, 'utf-8')
+        .then(content => {
+            this.productSave = JSON.parse(content);
+            this.productSave = this.productSave.filter(product => product.id != givenId);
+            console.log(this.productSave);
+        })
+        .catch(err => {console.log('Error de Lectura', err)})
     };
 
     deleteById(givenId){
-        this.productSave = this.productSave.filter(product => {
-            return product.id != givenId});
         
-        const productsText = JSON.stringify(this.productSave);
+        fs.promises.readFile(`./${this.archiveName}`, 'utf-8')
+        .then(content => {
+            this.productSave = JSON.parse(content);
+            this.productSave = this.productSave.filter(product => product.id != givenId);
+            console.log(this.productSave);
+            
+            const productsText = JSON.stringify(this.productSave);
 
-        fs.promises.writeFile(`./${this.archiveName}`, productsText)
+            try { fs.promises.writeFile(`./${this.archiveName}`, productsText)
+            }
+            catch (err) {
+                console.log('No se pudo escribir el archivo!')
+            }
+        })
+        .catch(err => {console.log('Error de Lectura', err)})
 
     };
 
     deleteAll(){
-        this.productSave = [];
-        
-        const productsText = JSON.stringify(this.productSave);
+        fs.promises.readFile(`./${this.archiveName}`, 'utf-8')
+        .then(content => {
+            this.productSave = JSON.parse(content);
+            this.productSave = [];
+            console.log(this.productSave);
+            
+            const productsText = JSON.stringify(this.productSave);
 
-        fs.promises.writeFile(`./${this.archiveName}`, productsText)
+            try { fs.promises.writeFile(`./${this.archiveName}`, productsText)
+            }
+            catch (err) {
+                console.log('No se pudo escribir el archivo!')
+            }
+        })
+        .catch(err => {console.log('Error de Lectura', err)})
     };
 }
 const container1 = new Container ('first-archive-products.txt');
 
-container1.save({item:'azucar',price:150});
+// container1.save({item:'azucar',price:150});
 
-container1.save({item:'fideos',price:80});
+// container1.save({item:'fideos',price:80});
 
 container1.save({item:'gaseosa',price:110});
 
 // container1.getAll();
 
-// container1.deleteById(2);
+// container1.deleteById(2349);
 
 // container1.getAll();
 
